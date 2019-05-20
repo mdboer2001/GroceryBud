5b302c3d7bced3547fbc6803bc0da1b42e48d3ac
TimmyGuy <tim.ohlsen2@gmail.com>
2019-05-20 14:30:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,33 @@
     <link href="css/custom.css" rel="stylesheet" type="text/css">
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
+      <div class="collapse navbar-collapse" id="navbarColor03">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <?php if(isset($username)) { echo $username; }else{ echo "Klant";} ?>
+          <a href="img/usr/user.png"><img src="img/usr/user.png"/></a>
+        </form>
+      </div>
+    </nav>
 
 
   </body>
