dc687317a6b7e93de71375a3ccc6cff10685ffee
joeykwispel <joey.oosenbrug@gmail.com>
2019-05-21 15:20:21 +0200

    index styling

--- index.php
+++ index.php
@@ -16,10 +16,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta charset="utf-8">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="css/custom.css" rel="stylesheet" type="text/css">
+    <style media="screen">
+      body{
+        background: none;
+      }
+    </style>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#"><img src="img/logo.png" style="width: 400px;" class="logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -29,14 +34,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <?php if(isset($username)) { echo $username; }else{ echo "Klant";} ?>
+          <?php if(isset($username)) { echo "Welkom: " , $username; }else{ echo "Klant";} ?>
 
           <?php/* if(isset($username)) { echo $username; }else{ echo "Klant";} */?>
           <div class="dropdown">
             <a href="#"><img src="img/usr/user.png" class="user_pic"></a>
             <div class="dropdown-content">
               <a href="profile.php">Persoonlijke informatie</a>
-              <a href="#">Wachtwoord veranderen</a>
+              <a href="#">Wachtwoord veranderen</a><br>
               <a href="logout.php">Log uit</a>
             </div>
           </div>
