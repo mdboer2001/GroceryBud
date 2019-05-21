c28be198eac96caaa696eda743a1b73e2423f913
TimmyGuy <tim.ohlsen2@gmail.com>
2019-05-21 09:40:50 +0200

    update inlog

---
+++ forgotpassword.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/custom.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    </head>
+    <body>
+        <form class="formulier" name="formulier" method="post">
+
+            <div class="contact">
+                <img src="img/logo.png" style="width: 300px;" class="logo">
+                <p class="welkom">Wachtwoord vergeten?</p>
+
+                <!--Naam-->
+                <div class="txt">
+                    <i class="fas fa-user icon"></i>
+                    <input type="text" name="name" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Gebruikersnaam of email" require>
+                </div>
+
+                <!--Email-->
+                <div class="txt">
+                    <i class="fas fa-key icon"></i>
+                    <input type="password" name="email" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Nieuw wachtwoord" require>
+                </div>
+
+                <div class="txt">
+                    <i class="fas fa-key icon"></i>
+                    <input type="password" name="email" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Bevestig nieuw wachtwoord" require>
+                </div>
+
+                <!--Bericht-->
+                <input type="submit" class="verder" value="Verder">
+                <br><br>
+                <a href="forgotusername.php">Gebruikersnaam vergeten?</a><br>
+                <a href="index.php">Login</a><br>
+
+            </body>
+</html>
--- login.php
+++ login.php
@@ -15,71 +15,37 @@ if (isset($_POST) && isset($_POST["email"])) {
   }
 }
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <style>
-    body {
-      width: 100vw;
-      height: 100vh;
-    }
-    label {
-      display: block;
-    }
-  </style>
-</head>
-<body style="background: linear-gradient(to right, red, yellow, lime, cyan, blue)">
-  <?php if (!isset($_SESSION["user"])): ?>
-  <form action="login.php" method="post">
-    <label>
-      email
-      <input type="text" name="email">
-    </label>
-    <label>
-      wachtwoord
-      <input type="text" name="pass">
-    </label>
-    <input type="submit" value="login">
-  </form>
-  <?php else: ?>
-  welkom <?php echo $_SESSION["user"]["naam"]; ?>
-  <form action="modify.php" method="post">
-    <label>
-      email
-      <input type="email" name="email" value="<?php echo $_SESSION["user"]["email"] ?>" required>
-    </label>
-    <label>
-      naam
-      <input type="text" name="naam" value="<?php echo $_SESSION["user"]["naam"] ?>" required>
-    </label>
-    <label>
-      adres
-      <input type="text" name="adres" value="<?php echo $_SESSION["user"]["adres"] ?>" required>
-    </label>
-    <label>
-      woonplaats
-      <input type="text" name="woonplaats" value="<?php echo $_SESSION["user"]["woonplaats"] ?>" required>
-    </label>
-    <label>
-      telefoonnummer
-      <input type="text" name="tel" value="<?php echo $_SESSION["user"]["tel"] ?>" required>
-    </label>
-    <label>
-      geboortedatum
-      <input type="date" name="geboortedatum" value="<?php echo $_SESSION["user"]["geboortedatum"] ?>" required>
-    </label>
-    <label>
-      wachtwoord
-      <input type="text" name="pass" placeholder="nieuw wachtwoord" required>
-    </label>
-    <input type="submit" value="verander gegevens">
-  </form>
-  <a href="logout.php">log uit</a>
-  <?php endif; ?>
-</body>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/custom.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    </head>
+    <body>
+        <form class="formulier" name="formulier" action="index.php" method="post">
+
+            <div class="contact">
+                <img src="img/logo.png" style="width: 300px;" class="logo">
+                <p class="welkom">Welkom <?php echo $_SESSION["user"]["naam"]; ?></p>
+
+
+                <!--Naam-->
+                <div class="txt">
+                    <i class="fas fa-user icon"></i>
+                    <input type="text" name="email" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Email" require>
+                </div>
+
+                <!--Email-->
+                <div class="txt">
+                    <i class="fas fa-key icon"></i>
+                    <input type="password" name="pass" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Wachtwoord" require>
+                </div>
+
+                <!--Bericht-->
+                <input type="submit" class="bttn" id="buttn" value="Login">
+                <br>
+                <a href="forgotusername.php">Gebruikersnaam vergeten?</a><br>
+                <a href="forgotpassword.php">Wachtwoord vergeten?</a><br><br>
+                <a href="register.php" class="register">Nog geen account? Meld je hier aan.</a>
+            </body>
 </html>
