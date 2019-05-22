2796ebb0106dfe45eb0d0e9b2bc535168451ee2b
WesleyvanRijn <1044077@mborijnland.nl>
2019-05-22 13:13:10 +0200

    verder gegaan met connect.php en index.php budget instellen

--- connect.php
+++ connect.php
@@ -1,9 +1,7 @@
 <?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$question = $_POST['question'];
-	$date =date("Y-m-d H:i:s");
+	$budget = $_POST['budget'];
 
-		$conn = mysqli_connect("localhost", "root", "", "otto") or die("We couldn't connect");
-		$success = mysqli_query($conn, "INSERT INTO faq(name, email, question, date) VALUES('$name' , '$email', '$question', '$date')")
+
+		$conn = mysqli_connect("localhost", "root", "", "grocerybud") or die("We couldn't connect");
+		$success = mysqli_query($conn, "INSERT INTO users ('budget') VALUES('$budget')");
 ?>
