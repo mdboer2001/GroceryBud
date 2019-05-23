b1956575bdd7123195f651c7d43a6736541c71ea
TimmyGuy <tim.ohlsen2@gmail.com>
2019-05-23 10:57:05 +0200

    Update grocerybud.sql

--- grocerybud.sql
+++ grocerybud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 22 mei 2019 om 13:39
--- Serverversie: 10.1.34-MariaDB
--- PHP-versie: 5.6.37
+-- Gegenereerd op: 23 mei 2019 om 10:55
+-- Serverversie: 10.1.37-MariaDB
+-- PHP-versie: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -39,10 +39,31 @@ CREATE TABLE `hoogvliet` (
 --
 
 INSERT INTO `hoogvliet` (`pr_id`, `pr_naam`, `pr_prijs`) VALUES
-(1, 'Tijgerbrood', 1.15),
-(2, 'Melk', 0.65),
-(3, 'Tijgerbrood', 1.15),
-(4, 'Melk', 0.65);
+(1, 'Tijgerbrood', 1.39),
+(2, 'Broccoli', 0.69),
+(3, 'Komkommer', 0.69),
+(4, 'Amstel Radler 0.0%', 3.89),
+(5, 'Ristorante Pizza Quattro Formaggi', 2.09),
+(6, 'Coca Cola 4pack', 7.95),
+(7, 'G\'woon Bronwater', 0.39),
+(8, 'Douwe Egberts Aroma rood donker filterkoffie', 3.18),
+(9, 'Alpro Amandeldrink ongezoet fresh', 1.79),
+(10, 'Kesbeke Augurken', 1.3),
+(26, 'Coca Cola 4pack', 7.95),
+(27, 'G\'woon Bronwater', 0.39),
+(28, 'Douwe Egberts Aroma rood donker filterkoffie', 3.18),
+(29, 'Alpro Amandeldrink ongezoet fresh', 1.79),
+(30, 'Kesbeke Augurken', 1.3),
+(31, 'Tijgerbrood', 1.39),
+(32, 'Broccoli', 0.69),
+(33, 'Komkommer', 0.69),
+(34, 'Amstel Radler 0.0%', 3.89),
+(35, 'Ristorante Pizza Quattro Formaggi', 2.09),
+(36, 'Coca Cola 4pack', 7.95),
+(37, 'G\'woon Bronwater', 0.39),
+(38, 'Douwe Egberts Aroma rood donker filterkoffie', 3.18),
+(39, 'Alpro Amandeldrink ongezoet fresh', 1.79),
+(40, 'Kesbeke Augurken', 1.3);
 
 -- --------------------------------------------------------
 
@@ -61,8 +82,16 @@ CREATE TABLE `jumbo` (
 --
 
 INSERT INTO `jumbo` (`pr_id`, `pr_naam`, `pr_prijs`) VALUES
-(1, 'Tijgerbrood', 1.35),
-(2, 'Melk', 0.8);
+(1, 'Tijgerbrood', 1.39),
+(2, 'Broccoli', 0.69),
+(3, 'Komkommer', 0.69),
+(4, 'Amstel Radler 0.0%', 3.5),
+(5, 'Ristorante Pizza Quattro Formaggi', 2.2),
+(6, 'Coca Cola 4pack', 7.95),
+(7, 'Bronwater', 0.15),
+(8, 'Douwe Egberts Aroma rood donker filterkoffie', 4),
+(9, 'Alpro Amandeldrink ongezoet fresh', 2),
+(10, 'Kesbeke Augurken', 1.95);
 
 -- --------------------------------------------------------
 
@@ -81,8 +110,16 @@ CREATE TABLE `lidl` (
 --
 
 INSERT INTO `lidl` (`pr_id`, `pr_naam`, `pr_prijs`) VALUES
-(1, 'Tijgerbrood', 1),
-(2, 'Melk', 0.5);
+(1, 'Tijgerbrood', 1.39),
+(2, 'Broccoli', 0.7),
+(3, 'Komkommer', 0.5),
+(4, 'Radler 0.0%', 2.45),
+(5, 'Ristorante Pizza Quattro Formaggi', 2.09),
+(6, 'Cola 6 pack 2L', 6.5),
+(7, 'Bronwater', 0.25),
+(8, 'Aroma rood donker filterkoffie', 3),
+(9, 'Alpro Amandeldrink ongezoet fresh', 1.79),
+(10, 'Kesbeke Augurken', 1.3);
 
 -- --------------------------------------------------------
 
@@ -104,8 +141,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_naam`, `password`, `email`, `budget`, `products`) VALUES
-(5, 'demo', '$2y$10$o792YnZO2sRAo4cSatzPBuwHDPfOpo9t4.BxI3spNXxx1oYA1tzN2', 'mikedeboer2001@hotmail.com', 0, ''),
-(6, 'demo', '$2y$10$USbK/Uvp/KCkr/Uf27RFUekcXdbvG3N24p8ijqs2xwp3gfpptSUwS', 'demo@demo.nl', 0, '');
+(7, 'Tim Ohlsen', '$2y$10$DLHs7s4em1TLiUtkTXqBru3tCw3qbW65woymJq3bfKVaRzTx0GT/C', 'tim.ohlsen2@gmail.com', 50, '');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -144,25 +180,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `hoogvliet`
 --
 ALTER TABLE `hoogvliet`
-  MODIFY `pr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `pr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT voor een tabel `jumbo`
 --
 ALTER TABLE `jumbo`
-  MODIFY `pr_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `pr_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `lidl`
 --
 ALTER TABLE `lidl`
-  MODIFY `pr_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `pr_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
